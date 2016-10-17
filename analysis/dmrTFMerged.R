2d8eb8b36302bfee88f7a1af16ba0b0228c8a398
Shreejoy Tripathy <stripat3@gmail.com>
2016-10-16 17:00:28 -0700

    adding comments for analysis of dmr x tf file

--- analysis/dmrTFMerged.R
+++ analysis/dmrTFMerged.R
@@ -7,10 +7,13 @@ data(TSS.human.GRCh37)
 # load in ChIP-Seq datasets for histone modifications - needs to be annotated to hg19
 # gets loaded in as genome ranges object
 
-# H3K4me3 ChIP-seq on human H1-hESC
+# load in file from fimo of up DMR regions
 fimo_up =  read.delim("~/hackseq/results/fimo_upmeth/fimo.txt")
 jaspar_names = read.delim("~/hackseq/results/JASPAR_NAMES", sep = " ", header = F)
 colnames(jaspar_names) = c("X.pattern.name", "TF_name")
+
+# merge fimo and file for jaspar names
 fimo_up = merge(fimo_up, jaspar_names, by = "X.pattern.name")
 
+# filter just runx1 TF
 runx1_up = fimo_up %>% filter(TF_name == 'RUNX1')
